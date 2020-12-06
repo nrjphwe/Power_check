13401b0491b37868925d4fe448422782989a570b
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2020-12-06 22:11:45 +0100

    Update power_check.py

--- power_check.py
+++ power_check.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+# /etc/init.d/power_check.py
+### BEGIN INIT INFO
+# Provides:          sample.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 import sys
 import mariadb
 import datetime #new
