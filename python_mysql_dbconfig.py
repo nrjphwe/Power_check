985601dfdf1cde47817a5356e4037f35111815da
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2020-12-23 23:55:01 +0100

    Update python_mysql_dbconfig.py

--- python_mysql_dbconfig.py
+++ python_mysql_dbconfig.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from configparser import ConfigParser
 
-def read_db_config(filename='config.ini', section='mysql'):
+def read_db_config(filename='/home/pi/Power_check/config.ini', section='mysql'):
     """ Read database configuration file and return a dictionary object
     :param filename: name of the configuration file
     :param section: section of database configuration
@@ -19,5 +19,4 @@ def read_db_config(filename='config.ini', section='mysql'):
             db[item[0]] = item[1]
     else:
         raise Exception('{0} not found in the {1} file'.format(section, filename))
-
     return db
